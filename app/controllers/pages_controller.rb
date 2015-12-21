9784c2de1d9631bfd3ae61fc70b21bf4e95476be
Kate Di Cerbo <katharine.dicerbo@gmail.com>
2015-12-21 15:49:29 +0000

    Added about page (view), Route, Controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
     def home
+        # This is a home action
+        # Now we create a file in pages (under view) that corresponds
     end
+    
+    def about
+    end 
 end
